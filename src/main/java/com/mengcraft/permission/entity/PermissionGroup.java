673df6fd07b75f7be61cb22376e33025b0bf9a3f
caoli5288 <caoli5288@gmail.com>
2015-10-26 23:47:32 +0800

    Commit

---
+++ src/main/java/com/mengcraft/permission/entity/PermissionGroup.java
@@ -0,0 +1,57 @@
+package com.mengcraft.permission.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created on 15-10-26.
+ */
+@Entity
+public class PermissionGroup {
+
+    @Id
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String value;
+
+    @Column
+    private boolean type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public boolean isType() {
+        return type;
+    }
+
+    public void setType(boolean type) {
+        this.type = type;
+    }
+
+}
